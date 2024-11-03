b55a3e60cd31162cfc6512df2c48f30a0ad8571a
yura d <yuriiakulinin@gmail.com>
2024-11-03 21:55:59 +0200

    adding index.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DGHDynamicMain from './components/dynamic/DGHDynamicMain';
 import DGHStaticMain from './components/static/DGHStaticMain';
+import './index.css';
 
 const App = () => {
     return (
